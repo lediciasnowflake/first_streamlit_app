046342de6f9578964a66b56fa10a91298cc019f1
lediciasnowflake <114475421+lediciasnowflake@users.noreply.github.com>
2022-09-27 17:33:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents new healthy dinner')
 streamlit.header('Breakfast Menu')
